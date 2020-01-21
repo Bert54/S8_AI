3ac20e5fe19a4bb40771c63bb09dd9bd9719f801
Bert54 <matt.bert@live.fr>
2020-01-21 13:44:08 +0100

    human can play moves

--- jeu_power4.c
+++ jeu_power4.c
@@ -16,6 +16,13 @@ typedef struct EtatSt {
 
 } State;
 
+typedef struct {
+
+    int column;
+
+} Move;
+
+
 State * init_state() {
     State * state = (State *)malloc(sizeof(State));
 
@@ -27,18 +34,52 @@ State * init_state() {
     return state;
 }
 
+Move * new_move(int i) {
+    Move * move = (Move *)malloc(sizeof(Move));
+    move->column = i;
+    return move;
+}
+
+Move * ask_move() {
+
+    int i;
+    printf(" Which column? ") ;
+    scanf("%d",&i);
+
+    return new_move(i);
+}
+
+int play_move( State * state, Move * move ) {
+
+    int i = WIDTH-2;
+    while(state->board[i][move->column] != ' ' && i >= 0) {
+        i--;
+    }
+    if (i >= WIDTH) {
+        return 0;
+    }
+    else {
+        state->board[i][move->column] = state->player ? 'O' : 'X';
+
+        // à l'autre joueur de jouer
+        //state->player = AUTRE_JOUEUR(etat->joueur);
+
+        return 1;
+    }
+}
+
 void print_game(State * state) {
 
     int i,j;
     printf("   |");
     for ( j = 0; j < WIDTH; j++)
-        printf(" %d |", j + 1);
+        printf(" %d |", j);
     printf("\n");
     printf("----------------");
     printf("\n");
 
     for(i=0; i < HEIGHT; i++) {
-        printf(" %d |", HEIGHT - i);
+        printf(" %d |", HEIGHT - i - 1);
         for ( j = 0; j < WIDTH; j++)
             printf(" %c |", state->board[i][j]);
         printf("\n");
@@ -52,8 +93,19 @@ void print_game(State * state) {
 int main() {
 
     State * state = init_state();
-
+    Move * move;
     print_game(state);
-
+    do {
+        move = ask_move();
+    } while ( !play_move(state, move) );
+    print_game(state);
+    do {
+        move = ask_move();
+    } while ( !play_move(state, move) );
+    print_game(state);
+    do {
+        move = ask_move();
+    } while ( !play_move(state, move) );
+    print_game(state);
     return 0;
 }
